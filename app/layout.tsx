1196223637a3261cefa981fb205274ff6fe22400
Vedang <vedangshetty21@gmail.com>
2025-03-19 11:48:44 +0530

    Styling Fragmented!!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./header.css";
+import "./hero.css";
+import "./mediastyle.css";
+import "./footer.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
